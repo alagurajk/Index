cf67ec6a19258ff6b68fd1e0be54193068e555f2
alagurajk <kamaraj5507@gmail.com>
2022-03-16 14:25:21 +0530

    new

---
+++ src/org/test/IndexValue.java
@@ -0,0 +1,27 @@
+package org.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IndexValue {
+	
+	
+	public static void main(String[] args) {
+		
+		List li= new ArrayList();
+		
+		li.add(10);
+		li.add(20);
+		li.add(30);
+		li.add(90);
+		System.out.println();
+		
+	int d =	li.indexOf(10);
+		System.out.println(d);
+		
+		
+	 	
+		
+	}
+}	
+
